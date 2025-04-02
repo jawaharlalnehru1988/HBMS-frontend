e59af14f6114e6853af95efdeefce62ef35aebe3
Jawaharlal Nehru <91001917+jawaharlalnehru1988@users.noreply.github.com>
2025-04-02 08:34:47 +0530

    Update auth.component.ts

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   imports: [],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+  registerForm: FormGroup;
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onRegister(): void {
+    if (this.registerForm.valid) {
+      console.log('Register', this.registerForm.value);
+    }
+  }
+
+  onLogin(): void {
+    if (this.loginForm.valid) {
+      console.log('Login', this.loginForm.value);
+    }
+  }
 }
